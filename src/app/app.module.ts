12729316101b30d47be429b4168672d2f4974b9e
lucasousi@gmail.com <lucasousi@gmail.com>
2021-07-05 01:17:14 -0300

    Finalizando primeira versao da tela de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
-import { NbLayoutModule, NbThemeModule } from '@nebular/theme';
+import { NbButtonModule, NbLayoutModule, NbThemeModule } from '@nebular/theme';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,6 +15,7 @@ import { AppComponent } from './app.component';
     BrowserAnimationsModule,
     NbThemeModule.forRoot({ name: 'default' }),
     NbLayoutModule,
+    NbButtonModule,
     NbEvaIconsModule,
   ],
   providers: [],
--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -1,12 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { NbButtonModule, NbFormFieldModule, NbIconModule, NbInputModule } from '@nebular/theme';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, LoginRoutingModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    LoginRoutingModule,
+    ReactiveFormsModule,
+    NbInputModule,
+    NbFormFieldModule,
+    NbIconModule,
+    NbButtonModule,
+  ],
 })
 export class LoginModule {}
