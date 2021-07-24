da70f404d47576c50b09150b38afed294a9688eb
lucasousi@gmail.com <lucasousi@gmail.com>
2021-07-24 20:49:34 -0300

    Primeira seção

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,12 +2,20 @@ import { NavbarModule } from 'app/components/navbar/navbar.module';
 
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NbButtonModule, NbIconModule } from '@nebular/theme';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule, NavbarModule],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    NavbarModule,
+    NbButtonModule,
+    RouterModule,
+  ],
 })
 export class HomeModule {}
