88526121932352010a60b853eb73f418d270732d
Lucas Simões <lucasousi@gmail.com>
2021-10-17 19:28:22 -0300

    Ajustes gerais de responsabilidade

--- src/app/components/card-tipo-emprestimo/card-tipo-emprestimo.component.ts
+++ src/app/components/card-tipo-emprestimo/card-tipo-emprestimo.component.ts
@@ -1,18 +1,28 @@
-import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnInit,
+} from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-card-tipo-emprestimo',
-  templateUrl: './card-tipo-emprestimo.component.html',
-  styleUrls: ['./card-tipo-emprestimo.component.scss'],
+  selector: "app-card-tipo-emprestimo",
+  templateUrl: "./card-tipo-emprestimo.component.html",
+  styleUrls: ["./card-tipo-emprestimo.component.scss"],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CardTipoEmprestimoComponent implements OnInit {
-  @Input() title = '';
-  @Input() subtitle = '';
+  @Input() title = "";
+  @Input() subtitle = "";
   @Input() features: string[] = [];
-  @Input() image = '';
+  @Input() image = "";
 
-  constructor() {}
+  constructor(private route: Router) {}
 
   ngOnInit(): void {}
+
+  goToRegisterPage() {
+    this.route.navigate(["cadastro"]);
+  }
 }
--- src/app/pages/selecao-tipo-emprestimo/selecao-tipo-emprestimo.component.ts
+++ src/app/pages/selecao-tipo-emprestimo/selecao-tipo-emprestimo.component.ts
@@ -1,10 +1,10 @@
-import { Location } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Location } from "@angular/common";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-selecao-tipo-emprestimo',
-  templateUrl: './selecao-tipo-emprestimo.component.html',
-  styleUrls: ['./selecao-tipo-emprestimo.component.scss'],
+  selector: "app-selecao-tipo-emprestimo",
+  templateUrl: "./selecao-tipo-emprestimo.component.html",
+  styleUrls: ["./selecao-tipo-emprestimo.component.scss"],
 })
 export class SelecaoTipoEmprestimoComponent implements OnInit {
   constructor(private _location: Location) {}
